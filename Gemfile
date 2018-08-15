4170401fdefb80af34dc0676d848aa8508249514
Slava C <veaceslav.chirita@gmail.com>
2018-08-15 06:30:34 +0300

    Changes

--- Gemfile
+++ Gemfile
@@ -47,8 +47,8 @@ group :development, :test do
 end
 
 group :development do
+  gem 'pg'
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
-  gem 'sqlite3'
   gem 'web-console', '>= 3.3.0'
   gem 'listen', '>= 3.0.5', '< 3.2'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,11 @@
 
 ActiveRecord::Schema.define(version: 2018_08_14_144908) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "photos", force: :cascade do |t|
-    t.integer "room_id"
+    t.bigint "room_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_file_name"
@@ -39,7 +42,7 @@ ActiveRecord::Schema.define(version: 2018_08_14_144908) do
     t.boolean "is_internet"
     t.integer "price"
     t.boolean "active"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "latitude"
@@ -74,4 +77,6 @@ ActiveRecord::Schema.define(version: 2018_08_14_144908) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "photos", "rooms"
+  add_foreign_key "rooms", "users"
 end
